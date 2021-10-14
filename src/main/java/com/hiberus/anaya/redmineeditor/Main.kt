f365dbe4dd9e7c5e7b5545657c72dec9975ab40a
Abel Naya <anaya@hiberus.com>
2021-10-14 17:38:22 +0200

    Conversion to Kotlin... 5) Main

---
+++ src/main/java/com/hiberus/anaya/redmineeditor/Main.kt
@@ -0,0 +1,25 @@
+package com.hiberus.anaya.redmineeditor
+
+import javafx.application.Application
+import javafx.fxml.FXMLLoader
+import javafx.scene.Scene
+import javafx.stage.Stage
+import java.io.IOException
+
+/**
+ * Main app
+ */
+fun main(args: Array<String>) = Application.launch(Main::class.java, *args)
+
+class Main : Application() {
+
+    @Throws(IOException::class)
+    override fun start(stage: Stage) = stage.run {
+        // load and show
+        title = "Redmine editor"
+        scene = Scene(FXMLLoader(this@Main.javaClass.getResource("parent.fxml")).load())
+        show()
+    }
+
+}
+
