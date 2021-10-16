f8591e70d1bf405684923fa5f6d536b2873f813b
Abel Naya <anaya@hiberus.com>
2021-10-16 19:22:39 +0200

    Conversion to Kotlin... 17) BaseComponent

--- src/main/java/com/hiberus/anaya/redmineeditor/components/BaseComponent.kt
+++ src/main/java/com/hiberus/anaya/redmineeditor/components/BaseComponent.kt
@@ -1,32 +1,33 @@
-package com.hiberus.anaya.redmineeditor.components;
+package com.hiberus.anaya.redmineeditor.components
 
-import com.hiberus.anaya.redmineeditor.controller.Controller;
+import com.hiberus.anaya.redmineeditor.controller.Controller
 
 /**
  * Just an interface for components, so that they can be populated with the controller from the parent (instead of using dependency injection)
+ * TODO: add dependency injection
  */
-abstract class BaseComponent {
+internal abstract class BaseComponent {
 
     /* ------------------------- bean injection ------------------------- */
 
     /**
      * The controller field
      */
-    Controller controller;
+    lateinit var controller: Controller
 
     /**
      * Initializes this component with the global controller
      *
      * @param controller global controller
      */
-    void injectController(Controller controller) {
-        this.controller = controller;
-        init();
+    fun injectController(controller: Controller) {
+        this.controller = controller
+        init()
     }
 
     /**
      * Initialization to do when the controller is populated
      */
-    abstract void init();
+    abstract fun init()
 
 }
