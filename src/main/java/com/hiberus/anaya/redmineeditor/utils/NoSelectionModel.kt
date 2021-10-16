5594003a7c5b1ffad9680d8ae552416f40208c99
Abel Naya <anaya@hiberus.com>
2021-10-17 00:35:25 +0200

    Conversion to Kotlin... 22) NoSelectionModel

---
+++ src/main/java/com/hiberus/anaya/redmineeditor/utils/NoSelectionModel.kt
@@ -0,0 +1,30 @@
+package com.hiberus.anaya.redmineeditor.utils
+
+import javafx.collections.FXCollections
+import javafx.collections.ObservableList
+import javafx.scene.control.MultipleSelectionModel
+
+/**
+ * Try to avoid selection in the custom listview
+ * From https://stackoverflow.com/a/46186195
+ * TODO: fix blue border on focus
+ *
+ * @param <T> Type of items
+ */
+class NoSelectionModel<T> : MultipleSelectionModel<T>() {
+    override fun getSelectedIndices(): ObservableList<Int> = FXCollections.emptyObservableList()
+    override fun getSelectedItems(): ObservableList<T> = FXCollections.emptyObservableList()
+    override fun selectIndices(index: Int, vararg indices: Int) {}
+    override fun selectAll() {}
+    override fun selectFirst() {}
+    override fun selectLast() {}
+    override fun clearAndSelect(index: Int) {}
+    override fun select(index: Int) {}
+    override fun select(obj: T) {}
+    override fun clearSelection(index: Int) {}
+    override fun clearSelection() {}
+    override fun isSelected(index: Int): Boolean = false
+    override fun isEmpty(): Boolean = true
+    override fun selectPrevious() {}
+    override fun selectNext() {}
+}
