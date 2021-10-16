05ab08c0ae32c5abaf1d61022639ec3a014e24fd
Abel Naya <anaya@hiberus.com>
2021-10-17 00:33:31 +0200

    Conversion to Kotlin... 21) ChangeEvents

--- src/main/java/com/hiberus/anaya/redmineeditor/model/ChangeEvents.kt
+++ src/main/java/com/hiberus/anaya/redmineeditor/model/ChangeEvents.kt
@@ -1,31 +1,38 @@
-package com.hiberus.anaya.redmineeditor.model;
+package com.hiberus.anaya.redmineeditor.model
 
 /**
  * Events corresponding to changes in the model
  */
-public enum ChangeEvents {
+enum class ChangeEvents {
+
     /**
      * The loading state changed
      */
     Loading,
+
     /**
      * The displayed month changed
      */
     Month,
+
     /**
      * The displayed day changed
      */
     Day,
+
     /**
      * Entries for the displayed day changed (not its content)
      */
     Entries,
+
     /**
      * Hours from displayed day were changed
      */
     Hours,
+
     /**
      * List of issues changed
      */
-    Issues,
+    Issues
+
 }
