25e5665ccc1d9d77d7fb61c9c6dd0cb434464c96
Abel Naya <anaya@hiberus.com>
2021-12-12 16:36:14 +0100

    fully configurable hours for each day (removed hardcoded)

--- src/main/java/com/hiberus/anaya/redmineeditor/utils/hiberus/Schedule.kt
+++ src/main/java/com/hiberus/anaya/redmineeditor/utils/hiberus/Schedule.kt
@@ -2,23 +2,19 @@ package com.hiberus.anaya.redmineeditor.utils.hiberus
 
 import com.hiberus.anaya.redmineeditor.utils.findFile
 import javafx.scene.paint.Color
+import java.security.InvalidParameterException
+import java.time.DayOfWeek
 import java.time.LocalDate
-import java.time.Month
 import java.time.YearMonth
 
 /**
  * the expected hours you were supposed to spend this day
  */
 val LocalDate.expectedHours
-    // if special day, return it
-    get() = SPECIAL.getOrElse(this) {
-        // otherwise, get default
-        when (month) {
-            // summer schedule
-            Month.JULY, Month.AUGUST -> doubleArrayOf(7.0, 7.0, 7.0, 7.0, 7.0, 0.0, 0.0)
-            // normal schedule
-            else -> doubleArrayOf(8.5, 8.5, 8.5, 8.5, 7.0, 0.0, 0.0)
-        }[dayOfWeek.value - 1] // 0 = monday, 6 = sunday
+    // tries caching value, otherwise calculates it
+    get() = CACHE.getOrPut(this) {
+        // get first match, 0 if none
+        RULES.firstOrNull { it.matches(this) }?.hours ?: 0.0
     }
 
 /**
@@ -57,41 +53,41 @@ fun getColor(expected: Double, spent: Double, day: LocalDate) = when {
  * Load special days from the configuration file
  */
 fun LoadSpecialDays() = runCatching {
-    SPECIAL.clear()
-    findFile("conf/special_days.conf").readLines().asSequence()
+    CACHE.clear()
+    RULES.clear()
+    findFile("conf/hours.conf").readLines().asSequence()
         // remove comments
         .map { it.replace("#.*".toRegex(), "") }
         // skip empty
         .filter { it.isNotBlank() }
         // split by comma
         .map { line -> line.split(",").map { it.trim() } to line }
-        // build valid to map
+        // build valid to list
         .mapNotNull { (data, line) ->
-            when {
-                data.size < 3 -> {
-                    // not enough
-                    System.err.println("Invalid entry in schedule, not enough data: $line")
-                    null
-                }
-                data.size == 3 -> {
-                    // year, month and day. 0 hours
-                    LocalDate.of(data[0].toInt(), data[1].toInt(), data[2].toInt()) to 0.0
-                }
-                else -> {
-                    if (data.size > 4) {
-                        // and other??
-                        println("Invalid entry in schedule, more than enough data: $line")
+            runCatching {
+                when {
+                    data.size < 3 -> {
+                        // not enough
+                        throw InvalidParameterException("not enough data: $line")
                     }
+                    data.size == 3 -> {
+                        // year, month and day. 0 hours
+                        Rule(data[0], data[1], data[2], 0.0)
+                    }
+                    else -> {
+                        if (data.size > 4) {
+                            // and other??
+                            throw InvalidParameterException("more than enough data: $line")
+                        }
 
-                    // year, month, day and hours
-                    LocalDate.of(data[0].toInt(), data[1].toInt(), data[2].toInt()) to data[3].toDouble()
+                        // year, month, day and hours
+                        Rule(data[0], data[1], data[2], data[3].toDouble())
+                    }
                 }
-            }
-        }.toMap()
-        // and save
-        .let {
-            SPECIAL.putAll(it)
-        }
+            }.onFailure { System.err.println("Invalid entry in hours file: \"${it.message}\"") }.getOrNull()
+        }.toList()
+        // and save (reversed)
+        .reversed().toCollection(RULES)
 }.onFailure {
     println(it)
     System.err.println("Special days file error!")
@@ -99,4 +95,32 @@ fun LoadSpecialDays() = runCatching {
 
 /* ------------------------- internal ------------------------- */
 
-private val SPECIAL = mutableMapOf<LocalDate, Double>()
+private val CACHE = mutableMapOf<LocalDate, Double>() // caching
+
+private val RULES = mutableListOf<Rule>() // save elements
+
+private class Rule(year: String, month: String, day: String, val hours: Double) {
+    private val year = if (year == "*") null else year.toInt()
+    private val month = if (month == "*") null else month.toInt()
+    private val day: Int?
+    private val week: DayOfWeek?
+
+    init {
+        // converts the day into either a day or a day-of-week (THINK: allow both?)
+        val (d, w) = listOf("m", "t", "w", "th", "f", "sa", "su").indexOf(day).takeIf { it != -1 }
+            ?.let { null to DayOfWeek.of(it + 1) } // day of week
+            ?: ((if (day == "*") null else day.toInt()) to null)  // normal day
+        this.day = d
+        this.week = w
+    }
+
+    /**
+     * true if the given date matches this rule
+     */
+    fun matches(date: LocalDate) =
+        (year?.let { date.year == it } ?: true)
+                && (month?.let { date.monthValue == it } ?: true)
+                && (day?.let { date.dayOfMonth == it } ?: true)
+                && (week?.let { date.dayOfWeek == it } ?: true)
+
+}
