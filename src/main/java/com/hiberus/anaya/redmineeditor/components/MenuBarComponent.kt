ca8f3e96a78939bf8c1e5d3ce96400b330eefa8f
Abel Naya <anaya@hiberus.com>
2022-02-15 17:59:24 +0100

    Full refactor for the fix month tool

--- src/main/java/com/hiberus/anaya/redmineeditor/components/MenuBarComponent.kt
+++ src/main/java/com/hiberus/anaya/redmineeditor/components/MenuBarComponent.kt
@@ -4,11 +4,13 @@ import com.hiberus.anaya.redmineeditor.Resources
 import com.hiberus.anaya.redmineeditor.dialogs.AppController
 import com.hiberus.anaya.redmineeditor.dialogs.BatchEditor
 import com.hiberus.anaya.redmineeditor.dialogs.DisplayEvidences
-import com.hiberus.anaya.redmineeditor.model.ChangeEvent
+import com.hiberus.anaya.redmineeditor.dialogs.ShowFixMonthDialog
 import com.hiberus.anaya.redmineeditor.utils.*
 import javafx.fxml.FXML
-import javafx.scene.control.*
-import java.time.LocalDate
+import javafx.scene.control.Alert
+import javafx.scene.control.ButtonType
+import javafx.scene.control.Label
+import javafx.scene.control.ScrollPane
 
 /**
  * the top menubar with actions
@@ -54,44 +56,6 @@ internal class MenuBarComponent {
     fun batchEditor() = BatchEditor()
 
     @FXML
-    fun fixMonth() {
-        ChoiceDialog(null, AppController.runForeground { it.loadedIssues })
-            .apply {
-                contentText = """
-                    With this tool you can 'fix' the whole selected month (past days only) in just a few seconds:
-                    - Any day where spent hours is the same (=) as the expected will not be changed
-                    - Any day where spent hours is greater (>) than expected, all its entries will be reduced equally (If you should have spent 8 hours but you spent [2, 6, 2] = 10 then 2 hours will be reduced accordingly -> [1.6, 4.8, 1.6] = 8
-                    - On any day where spent hours is less (<) than expected, a new entry for the selected issue with the selected comment will be created with the required hours
-                     
-                     After the tool runs you need to press upload to save the changes. In other words, you can run this tool and then reload without modifying anything, for testing.
-                """.trimMargin()
-            }
-            .showAndWait().ifPresent { issue ->
-                AppController.runBackground { model ->
-                    val entries = model.monthEntries ?: return@runBackground
-
-                    model.month.days()
-                        .filter { it <= LocalDate.now() }
-                        .forEach { day ->
-                            val dayEntries = entries.filter { it.spent_on == day }
-                            val expected = day.expectedHours
-                            val spent = dayEntries.sumOf { it.spent }
-                            if (expected - spent > 0) {
-                                // create entry
-                                model.createTimeEntry(issue = issue, spent = expected - spent, date = day)
-                            } else if (expected - spent < 0) {
-                                // remove extra time
-                                dayEntries.forEach {
-                                    it.changeSpent(it.spent / spent * expected)
-                                    model.registerExternalChange(ChangeEvent.EntryContent)
-                                    model.registerExternalChange(ChangeEvent.DayHours)
-                                    model.registerExternalChange(ChangeEvent.MonthHours)
-                                }
-                            }
-
-                        }
-                }
-            }
-    }
+    fun fixMonth() = ShowFixMonthDialog()
 
 }
--- src/main/java/com/hiberus/anaya/redmineeditor/dialogs/FixMonth.kt
+++ src/main/java/com/hiberus/anaya/redmineeditor/dialogs/FixMonth.kt
@@ -1,23 +1,22 @@
 package com.hiberus.anaya.redmineeditor.dialogs
 
-import com.hiberus.anaya.redmineapi.Redmine
+import com.hiberus.anaya.redmineapi.Issue
 import com.hiberus.anaya.redmineapi.TimeEntry
 import com.hiberus.anaya.redmineeditor.Resources
-import com.hiberus.anaya.redmineeditor.model.AppSettings
-import com.hiberus.anaya.redmineeditor.utils.*
-import javafx.application.Platform
-import javafx.event.EventHandler
+import com.hiberus.anaya.redmineeditor.model.ChangeEvent
+import com.hiberus.anaya.redmineeditor.utils.days
+import com.hiberus.anaya.redmineeditor.utils.expectedHours
+import com.hiberus.anaya.redmineeditor.utils.stylize
 import javafx.fxml.FXML
 import javafx.fxml.FXMLLoader
-import javafx.scene.Node
 import javafx.scene.Scene
-import javafx.scene.control.*
-import javafx.scene.paint.Color
+import javafx.scene.control.Alert
+import javafx.scene.control.ChoiceBox
+import javafx.scene.control.TextField
 import javafx.stage.Modality
 import javafx.stage.Stage
 import javafx.stage.WindowEvent
-import java.util.*
-import kotlin.concurrent.thread
+import java.time.LocalDate
 
 /**
  * Displays the Fix Month tool dialog
@@ -44,214 +43,68 @@ class FixMonthController {
     /* ------------------------- nodes ------------------------- */
 
     @FXML
-    lateinit var parent: Node // parent node
-    private val window get() = parent.scene.window // window
+    lateinit var comment: TextField
 
     @FXML
-    lateinit var domain: TextField // domain setting
+    lateinit var issue: ChoiceBox<Issue>
 
-    @FXML
-    lateinit var predefined: MenuButton
-
-    @FXML
-    lateinit var key: PasswordField // key setting
-
-    @FXML
-    lateinit var testLoading: ProgressIndicator // loading indicator for testing api
-
-    @FXML
-    lateinit var testInfo: Label // info about api test
-
-    @FXML
-    lateinit var allowGetOnly: CheckBox // allow get only setting
-
-    @FXML
-    lateinit var autoLoadTotal: CheckBox // autoload total hours setting
-
-    @FXML
-    lateinit var prevDays: Spinner<Int> // number of previous days setting
-
-    @FXML
-    lateinit var dark: CheckBox // dark theme setting
-
-    @FXML
-    lateinit var save: Button // save button
+    private val window get() = comment.scene.window // window
 
     /* ------------------------- functions ------------------------- */
 
     @FXML
     fun initialize() {
-        // register callback to closing event
-        Platform.runLater {
-            window.setOnCloseRequest { closeWindowEvent() }
-        }
-
+        // get issues
         val issues = AppController.runForeground { it.loadedIssues }
 
-        // prepare testLoading
-        testLoading.run {
-            syncInvisible()
-            isVisible = false
+        if (issues == null) {
+            // not loaded yet
+            Alert(Alert.AlertType.ERROR, "Redmine is not loaded").apply {
+                stylize()
+            }.showAndWait()
+            return
         }
 
-        // syn dark setting with dark theme
-        dark.selectedProperty().addListener { _, _, isDark ->
-            parent.scene?.stylize(isDark)
-        }
+        // assign issues
+        issue.items += issues.sortedByDescending { it.spent }
+        issue.selectionModel.select(0)
 
-        // predefined options
-        with(predefined) {
-            // get from file, if exists
-            val options = Properties().apply { findFile("conf/predefined.properties").takeIf { it.exists() }?.inputStream()?.use { load(it) } }
-            if (options.isEmpty) {
-                // no entries, hide button
-                syncInvisible()
-                isVisible = false
-            } else {
-                // entries, add as menus
-                items += options.map { (name, value) ->
-                    MenuItem(name as String?).apply {
-                        onAction = EventHandler {
-                            domain.text = value as String?
-                        }
+    }
+
+    @FXML
+    fun run() = AppController.runBackground({ model ->
+        // get entries
+        val entries = model.monthEntries ?: return@runBackground
+
+        // for each past day
+        model.month.days()
+            .filter { it <= LocalDate.now() }
+            .forEach { day ->
+                // get data of that day
+                val dayEntries = entries.filter { it.spent_on == day }
+                val expected = day.expectedHours
+                val spent = dayEntries.sumOf { it.spent }
+                if (expected - spent > 0) {
+                    // pending hours, create entry
+                    model.createTimeEntry(issue = issue.selectionModel.selectedItem, comment = comment.text, spent = expected - spent, date = day)
+                } else if (expected - spent < 0) {
+                    // extra hours, remove time
+                    dayEntries.forEach {
+                        it.changeSpent(it.spent / spent * expected)
+                        model.registerExternalChange(ChangeEvent.EntryContent)
+                        model.registerExternalChange(ChangeEvent.DayHours)
+                        model.registerExternalChange(ChangeEvent.MonthHours)
                     }
                 }
             }
-        }
-
-        // initialize properties
-        domain.text = AppSettings.URL.value
-        key.text = AppSettings.KEY.value
-        allowGetOnly.isSelected = AppSettings.READ_ONLY.value.toBoolean()
-        autoLoadTotal.isSelected = AppSettings.AUTO_LOAD_TOTAL_HOURS.value.toBoolean()
-        prevDays.valueFactory.value = AppSettings.PREV_DAYS.value.toInt()
-        dark.isSelected = AppSettings.DARK_THEME.value.toBoolean()
-
-        // intelligent save button
-        with({
-            // check and update lambda
-            val changes = changes()
-            save.text = when {
-                ReloadSettings intersects changes -> "_Save & reload"
-                changes.isNotEmpty() -> "_Save & apply"
-                else -> "_Save"
-            }
-            save.enabled = changes.isNotEmpty()
-        }) {
-            // apply to all properties
-            listOf(
-                domain.textProperty(),
-                key.textProperty(),
-                allowGetOnly.selectedProperty(),
-                autoLoadTotal.selectedProperty(),
-                prevDays.valueProperty(),
-                dark.selectedProperty()
-            ).forEach {
-                it.addListener { _, _, _ -> this() }
-            }
-            this()
-        }
+    }) {
+        // when correctly finishes, exit
+        if (it) cancel()
     }
 
-    @FXML
-    fun instructions() {
-        // show instructions to fill the key
-        Alert(Alert.AlertType.INFORMATION).apply {
-            title = "API key instructions"
-            headerText = "Fill this value with your Redmine API key"
-            dialogPane.contentText = "You can find it in Redmine -> my page -> api key."
-
-            // set buttons
-            clearButtons()
-            val openButton = addButton(ButtonType("Open")) {
-                openInBrowser(domain.text.ensureSuffix("/") + "my/api_key")
-            }
-            addButton(ButtonType.CLOSE)
-
-            // disable open if no domain
-            if (domain.text.isBlank()) {
-                openButton.enabled = false
-                dialogPane.contentText += "\n\n(note: fill the domain and open these instructions again to enable the open button)"
-            }
-
-            stylize(dark.isSelected)
-        }.showAndWait() // display
-    }
-
-    @FXML
-    fun testAPI() {
-        // test domain and key
-        testLoading.isVisible = true
-        testInfo.text = ""
-        testInfo.backgroundColor = null
-
-        // run in background
-        thread(isDaemon = true) {
-            val result = runCatching {
-                "OK: Valid settings found for user " + Redmine(domain.text, key.text, 0).getUserName(appendLogin = true)
-            }.getOrNull()
-
-            // then notify in foreground
-            Platform.runLater {
-                testInfo.text = result ?: "ERROR: Invalid settings, can't connect to service"
-                testInfo.backgroundColor = if (result != null) Color.LIGHTGREEN else Color.INDIANRED
-                testLoading.isVisible = false
-            }
-        }
-    }
-
-    @FXML
-    fun loadDefault() {
-        // load default settings, but ask first
-        Alert(Alert.AlertType.CONFIRMATION, "Do you want to discard all data and load all default settings?")
-            .apply {
-                stylize(dark.isSelected)
-                addButton(ButtonType.OK) {
-                    domain.text = AppSettings.URL.default
-                    key.text = AppSettings.KEY.default
-                    allowGetOnly.isSelected = AppSettings.READ_ONLY.default.toBoolean()
-                    autoLoadTotal.isSelected = AppSettings.AUTO_LOAD_TOTAL_HOURS.default.toBoolean()
-                    prevDays.valueFactory.value = AppSettings.PREV_DAYS.default.toInt()
-                    dark.isSelected = AppSettings.DARK_THEME.default.toBoolean()
-                }
-            }.showAndWait()
-    }
-
-    @FXML
-    fun save() {
-        // save settings
-        window.userData = changes(apply = true)
-        // and exit
-        window.hide()
-    }
 
     @FXML
     // pressing cancel is the same as pressing the 'x' close button
     fun cancel() = window.fireEvent(WindowEvent(window, WindowEvent.WINDOW_CLOSE_REQUEST))
-
-/* ------------------------- internal ------------------------- */
-
-    /**
-     * List of changes
-     */
-    private fun changes(apply: Boolean = false) =
-        listOf(
-            AppSettings.URL to domain.text,
-            AppSettings.KEY to key.text,
-            AppSettings.READ_ONLY to allowGetOnly.isSelected,
-            AppSettings.AUTO_LOAD_TOTAL_HOURS to autoLoadTotal.isSelected,
-            AppSettings.PREV_DAYS to prevDays.valueFactory.value,
-            AppSettings.DARK_THEME to dark.isSelected,
-        ).filter { (setting, value) ->
-            if (apply) setting.modify(value)
-            else setting.value != value.toString()
-        }.map { it.first }.toSet()
-
-    /**
-     * On window closes, asks to lose changes if any
-     */
-    private fun closeWindowEvent() {
-        if (changes().isNotEmpty() && !confirmLoseChanges("exit")) window.hide()
-    }
 
 }
