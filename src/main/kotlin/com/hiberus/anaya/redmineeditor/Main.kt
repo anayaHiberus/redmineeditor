fa75bba89597849c9b5f3193d72e4146cbebb735
Abel <anaya@hiberus.com>
2024-06-03 09:28:28 +0200

    remove duplicated old command management

--- src/main/kotlin/com/hiberus/anaya/redmineeditor/Main.kt
+++ src/main/kotlin/com/hiberus/anaya/redmineeditor/Main.kt
@@ -5,7 +5,6 @@ import com.hiberus.anaya.redmineeditor.dialogs.TITLE
 import com.hiberus.anaya.redmineeditor.utils.centerInMouseScreen
 import com.hiberus.anaya.redmineeditor.utils.stylize
 import javafx.application.Application
-import javafx.application.Platform
 import javafx.fxml.FXMLLoader
 import javafx.scene.Scene
 import javafx.stage.Stage
@@ -13,11 +12,10 @@ import javafx.stage.Stage
 /** iff we are running on headless mode (without a UI) */
 var HEADLESS = true
 
-/**
- * If this is specified, the help is shown and nothing else runs
- */
+/** If this is specified, the help is shown and nothing else runs */
 const val HELP_FLAG = "-h"
 
+/** Starts the app. */
 class Main {
     companion object {
         @JvmStatic
@@ -72,61 +70,8 @@ class Main {
     }
 }
 
-/**
- * Main app
- */
+/** Starts the window (stage) */
 class App : Application() {
-
-    /**
-     * Starts the app process
-     */
-    override fun init() {
-
-        // help flag
-        if (HELP_FLAG in parameters.raw) {
-            // get requested commands
-            COMMANDS.filter { it.argument in parameters.raw }.ifEmpty {
-                // show all help
-                println("These are all the available commands:")
-                COMMANDS
-            }.forEach { command ->
-                println()
-                println(buildString {
-                    append(command.name)
-                    append(": call as ")
-                    append(command.argument)
-                    if (command.parameters.isNotBlank()) append(" ").append(command.parameters)
-                    if (command.skipUI) append(" {will not run the UI afterwards}")
-                })
-                println(command.help.joinToString("\n") { "    $it" })
-            }
-            // and exit
-            Platform.exit()
-            return
-        }
-
-        // run valid commands
-        COMMANDS.filter { it.argument in parameters.raw }
-            .onEach {
-                println("Running ${it.name} (${it.argument} found)${if (it.skipUI) " [the UI won't be shown afterwards]" else ""}:")
-                it.run(parameters)
-                println()
-            }.run {
-                if (any { it.skipUI }) {
-                    // and exit if required
-                    Platform.exit()
-                    return
-                }
-            }
-
-        // launch UI
-        println("Launching UI (run with $HELP_FLAG to see a list of commands)")
-    }
-
-
-    /**
-     * Starts the app window (stage)
-     */
     override fun start(stage: Stage) = stage.run {
         // load and show
         title = TITLE
@@ -135,5 +80,4 @@ class App : Application() {
         centerInMouseScreen()
         show()
     }
-
 }
